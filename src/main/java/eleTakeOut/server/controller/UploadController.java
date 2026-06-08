48e988bd78d3e16aac3465f8055a0d9399e31523
shi-changyu123 <1163481801@qq.com>
2026-06-08 21:31:54 +0800

    整个后端共107个主要文件，基础代码和逻辑功能全部完成

--- src/main/java/eleTakeOut/server/controller/UploadController.java
+++ src/main/java/eleTakeOut/server/controller/UploadController.java
@@ -3,21 +3,27 @@ package eleTakeOut.server.controller;
 
 import eleTakeOut.common.result.Result;
 import eleTakeOut.common.utils.AliyunOSSOperator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @Slf4j
 @RestController
+@RequestMapping("/upload")
+@Tag(name = "文件上传模块")
+@RequiredArgsConstructor
 public class UploadController {
+    private final AliyunOSSOperator aliyunOSSOperator;
 
-    @Autowired
-    private AliyunOSSOperator aliyunOSSOperator;
-
-    @PostMapping("/upload")
+    @PostMapping
+    @Operation(summary = "文件上传")
     public Result upload(MultipartFile file) throws Exception {
         log.info("文件上传: {}", file.getOriginalFilename());
 
